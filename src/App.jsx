8fc2ab58a94f28a080238b3ba7403e00c54c181c
CharlyCodigo <juancarlos45836@gmail.com>
2023-01-27 15:50:14 -0600

    Clear App

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-     
+    <div class="App">
+    
     </div>
   )
 }
